12d5651201b9961ff2d058618e66a84547fe82a2
kosmos4v <kosmos4v@gmail.com>
2023-06-07 03:04:26 +0300

    fix: fixed saga functions, mede enum types for api methods and fetch methods, add proxy to package_json

--- src/utils/requests/request.ts
+++ src/utils/requests/request.ts
@@ -1,28 +1,28 @@
-const BASE_URL = 'https://api.green-api.com';
-
 type TBody = Record<string, string>;
+
+export enum ApiMethod {
+  sendMessage = 'sendMessage',
+  receiveNotification = 'receiveNotification',
+  deleteNotification = 'deleteNotification',
+}
 
 export function request(
   method: string,
-  apiMethod: string,
+  apiMethod: ApiMethod,
   body?: TBody,
   receiptId?: number,
 ): Promise<Response> {
   const userApiToken: string | null = localStorage.getItem('userApiToken');
   const userId: string | null = localStorage.getItem('userId');
 
+  if (!userApiToken || !userId) return Promise.reject();
+
   const url = userApiToken && userId
-    ? `${BASE_URL}/waInstance${userId}/${apiMethod}/${userApiToken}`
+    ? `/waInstance${userId}/${apiMethod}/${userApiToken}`
     : '';
 
   const headers: Record<string, string> = {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:3000, http://localhost:3000/home',
-    'User-Agent': 'PostmanRuntime/7.32.2',
-    Connection: 'keep-alive',
-    'Accept-Encoding': 'gzip, deflate, br',
-    Accept: '*/*',
-    mode: 'no-cors',
   };
   return fetch(
     receiptId ? `${url}/${receiptId}` : url,
